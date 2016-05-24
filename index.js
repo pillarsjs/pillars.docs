2f020bead2b9bf22ce5e0bd6b1af104a0b2a85eb
Javier GM <bifuer@gmail.com>
2016-05-24 14:28:11 +0200

    new template default engines apply

--- index.js
+++ index.js
@@ -31,49 +31,6 @@ i18n.languages = ['es'];
 // Log manager config
 var crier = global.crier.addGroup('pillarsdocs');
 
-// Setup Templated .jade support.
-
-var marked = require('marked');
-marked.setOptions({
-  highlight: function (code,lang) {
-    return hljsFix(code,lang);
-  }
-});
-
-var hljs = require('highlight.js');
-function hljsFix(str,lang){
-  var result;
-  if(lang){
-    result = hljs.highlight(lang,str,true).value;
-  } else {
-    result = hljs.highlightAuto(str).value;
-  }
-  result = result.replace(/^((<[^>]+>|\s{4}|\t)+)/gm, function(match, r) {
-    return r.replace(/\s{4}|\t/g, '  ');
-  });
-  result = result.replace(/\n/g, '<br>');
-  return '<pre class="highlight"><code>'+result+'</pre></code>';
-}
-
-var jade = require('jade');
-jade.filters.highlight = function(str,opts){
-  return hljsFix(str,opts.lang);
-};
-jade.filters.marked = function(str,opts){
-  return marked(str,opts);
-};
-jade.filters.codesyntax = function(str,opts){
-  str = str.replace(/^((<[^>]+>|\s{4}|\t)+)/gm, function(match, r) {
-    return r.replace(/\s{4}|\t/g, '  ');
-  });
-  return '<pre class="codesyntax"><code>'+str+'</pre></code>';
-};
-
-var templated = global.templated;
-templated.addEngine('jade',function compiler(source,path){
-  return jade.compile(source,{filename:path,pretty:false,debug:false,compileDebug:false});
-});
-
 
 // Static service
 var pillarsDocsStatic = new Route({
